ecc74d9a66cb0334a19934dc5246ed819de7dfc5
omegatro <omegatro@gmail.com>
2024-01-21 13:42:22 +0200

    Fixing saving paths
    1. Added unique naming for the models for more convenient experimentation.
    2. Added unique naming for visualizations for more convenient experimentation.

--- modules/modeling.py
+++ modules/modeling.py
@@ -1,4 +1,8 @@
 import gensim.models
+import pyLDAvis.gensim
+import pickle 
+import pyLDAvis
+import os
 
 '''
 Development notes
@@ -22,3 +26,23 @@ class LatentDirichletAllocation():
         return gensim.models.LdaMulticore(corpus=corpus,
                                        id2word=vocab,
                                        num_topics=num_topics)
+    
+
+    @staticmethod
+    def load_lda_model(path):
+        return gensim.models.LdaMulticore.load(path)
+    
+
+    @staticmethod
+    def visualize_lda_model(model, corpus, vocab, output_path):
+        # Visualize the topics
+        # # this is a bit time consuming - make the if statement True
+        # # if you want to execute visualization prep yourself
+        if not os.path.isfile(output_path):
+            LDAvis_prepared = pyLDAvis.gensim.prepare(model, corpus, vocab)
+            viz_path = os.path.abspath(os.path.join(output_path, 'phyloviz'))
+            os.makedirs(viz_path,exist_ok=True)
+            viz_path= os.path.join(viz_path, 'viz_' + str(model.num_topics) + '.pickle')
+            with open(viz_path, 'wb') as f:
+                pickle.dump(LDAvis_prepared, f)
+            pyLDAvis.save_html(LDAvis_prepared, os.path.join(output_path,str(model.num_topics) +'.html'))
