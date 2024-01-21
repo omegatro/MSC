2c81b608d6595ac1014d12b0d232101ed8c8ec10
omegatro <omegatro@gmail.com>
2024-01-21 13:43:25 +0200

    Adjustments
    Adjusted to fit the updated code of the modules.

--- main.py
+++ main.py
@@ -10,20 +10,22 @@ def main():
     interface   = cmdi()
     args        = interface.parse_arguments(argument_dict)
     zc          = elc.connect_zotero(library_id=LIB_ID, api_key=API_KEY, library_type='user')
-    mapping     = elc.map_colname_colid_zotero(zotero_connection=zc)
-    items       = elc.get_items_zotero(zc, mapping, subset=[args.l])
-    pdf_url_map = elc.get_pdf_urls_zotero(items)
+    mapping     = elc.map_colname_colid_zotero(zotero_connection=zc, collection_name=args.c, depth=int(args.d))
+    items       = elc.get_items_zotero(zc, mapping)
+    pdf_url_map = elc.get_pdf_urls_zotero(items, col_name=args.c)
+    
     elc.download_files(args.o, pdf_url_map)
     pdf_gen     = pp.preprocess_generator(tp.pdf_generator(args.o))
     vocab       = pp.gen_vocab(pdf_gen)
     bow_gen     = pp.bow_generator(pp.preprocess_generator(tp.pdf_generator(args.o)), vocab=vocab)
     corpus      = [doc for doc in bow_gen]
-    if not os.path.isfile('./models/lda.model'):
+    model_path  = f'./models/{args.m}.model'
+    if not os.path.isfile(model_path):
         lda_model   = lda.get_lda_model(corpus=corpus, vocab=vocab, num_topics=3)
-        lda_model.save('./models/lda.model')
+        lda_model.save(model_path)
     else:
-        lda_model = lda.load_lda_model('./models/lda.model')
-    lda.visualize_lda_model(model=lda_model, corpus = corpus, vocab=vocab)
+        lda_model = lda.load_lda_model(model_path)
+    lda.visualize_lda_model(model=lda_model, corpus = corpus, vocab=vocab, output_path=os.path.join(args.o,args.m))
 
 
 
